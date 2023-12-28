756fa1796e31ce8cb420580e98d18a5182d728cb
julio-cesarcs <julio_cezzar@yahoo.com.br>
2023-12-27 22:52:45 -0300

    inclusão de arquivos

--- assets/js/enlarge.js
+++ assets/js/enlarge.js
@@ -1,12 +1,15 @@
 const divImage = document.querySelector('.panel')
+const body = document.querySelector('body')
 
 function enlargedImage(imagem){
     const bigImage = document.querySelector('.enlarged-image img')
     bigImage.src = imagem.target.attributes.src.nodeValue
     divImage.style.display = 'block'
+    body.style.overflow = 'hidden'
 }
 
 const btn = document.querySelector('.close-image')
 btn.addEventListener('click', ()=>{
     divImage.style.display = 'none'
+    body.style.overflow = 'visible'
 })
